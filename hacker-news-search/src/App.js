bdd9362e7a988c54a544dd50b46bc2b354b84148
TrentSchneider <trentonschneider@gmail.com>
2021-06-17 15:03:12 -0400

    Added history to props passed to home componenet

--- hacker-news-search/src/App.js
+++ hacker-news-search/src/App.js
@@ -19,6 +19,7 @@ function App() {
             <Home
               searchResult={searchResult}
               setSearchResult={setSearchResult}
+              history={history}
               setHistory={setHistory}
             />
           </Route>
