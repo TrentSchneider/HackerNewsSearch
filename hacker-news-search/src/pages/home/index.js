2c72371cd988bc3671677b035140e6e29d71035f
TrentSchneider <trentonschneider@gmail.com>
2021-06-17 14:24:41 -0400

    Imported React, set up searchText state and created frame for the search function

---
+++ hacker-news-search/src/pages/home/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+function Home() {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearchClick() {}
+}
+
+export default Home;
