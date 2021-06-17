2c036cd41c0e5f85ae684843d126aa0f32c8f5b2
TrentSchneider <trentonschneider@gmail.com>
2021-06-17 13:42:55 -0400

    Added in header and footer into page layout

--- hacker-news-search/src/App.js
+++ hacker-news-search/src/App.js
@@ -1,15 +1,21 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div>
-    <Router>
-      <Switch>
-        <Route exact path={["/","/home"]}></Route>
-        <Route exact path={["/history"]}></Route>
-      </Switch>
-    </Router>
-  </div>;
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path={["/", "/home"]}></Route>
+          <Route exact path={["/history"]}></Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
