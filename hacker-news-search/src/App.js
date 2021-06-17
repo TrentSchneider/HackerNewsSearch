290608b3e397d65271650e12f09a197daab5525d
TrentSchneider <trentonschneider@gmail.com>
2021-06-16 23:23:46 -0400

    Added initial React Router, Switch, and Routes

--- hacker-news-search/src/App.js
+++ hacker-news-search/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div>Placeholder</div>
-  );
+  return <div>
+    <Router>
+      <Switch>
+        <Route exact path={["/","/home"]}></Route>
+        <Route exact path={["/history"]}></Route>
+      </Switch>
+    </Router>
+  </div>;
 }
 
 export default App;
