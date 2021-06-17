c1449a5c0965ce446d83e66eeae6faad9f8cb779
TrentSchneider <trentonschneider@gmail.com>
2021-06-17 17:13:28 -0400

    Updated http to https in queryURL to avoid CORS

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,13 +3,13 @@ import axios from "axios";
 export default {
   search: function (storySearch) {
     let queryURL =
-      "http://hn.algolia.com/api/v1/search?query=" +
+      "https://hn.algolia.com/api/v1/search?query=" +
       storySearch +
       "&tags=story";
     return axios.get(queryURL);
   },
   recent: function () {
-    const queryURL = "http://hn.algolia.com/api/v1/search_by_date?tags=story";
+    const queryURL = "https://hn.algolia.com/api/v1/search_by_date?tags=story";
     return axios.get(queryURL);
   }
 };
