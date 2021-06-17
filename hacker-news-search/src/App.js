8bb1ed99b46bfdc8bf8f7bb15bda2da1d22a0316
TrentSchneider <trentonschneider@gmail.com>
2021-06-17 14:23:50 -0400

    Inserted Home  History components into Routes and added initial states

--- hacker-news-search/src/App.js
+++ hacker-news-search/src/App.js
@@ -1,16 +1,30 @@
+import React, { useEffect, useState } from "react";
+import Home from "./pages/home";
+import History from "./pages/history";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const [searchResult, setSearchResult] = useState([]);
+  const [history, setHistory] = useState([]);
+
   return (
     <div>
       <Router>
         <Header />
         <Switch>
-          <Route exact path={["/", "/home"]}></Route>
-          <Route exact path={["/history"]}></Route>
+          <Route exact path={["/", "/home"]}>
+            <Home
+              searchResult={searchResult}
+              setSearchResult={setSearchResult}
+              setHistory={setHistory}
+            />
+          </Route>
+          <Route exact path={["/history"]}>
+            <History history={history} />
+          </Route>
         </Switch>
         <Footer />
       </Router>
