7dcbde66bc5d72b57d2604d3dab30403c85b96b5
TrentSchneider <trentonschneider@gmail.com>
2021-06-18 00:24:18 -0400

    Set up if/else for search results based on response and set up object array mapping for results array

---
+++ src/components/searchResult/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function SearchResult(props) {
+  if (props.searchResult.length === 0) {
+    return (
+      <div>
+        <h3>No Results</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Results:</h2>
+        <div className="card">
+          test2
+          {props.searchResult.map((data, i) => {
+            return (
+              <div key={i}>
+                test
+                <div>
+                  <a href={data.url} target="_blank" rel="noreferrer">
+                    <h4>Title {data.title}</h4>
+                    <h5>Author {data.author}</h5>
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchResult;
