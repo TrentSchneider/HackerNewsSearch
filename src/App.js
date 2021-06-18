8b6f254627ee1cc73125578403864553ffa31780
TrentSchneider <trentonschneider@gmail.com>
2021-06-18 16:40:08 -0400

    Moved states and handleSearchClick function to App

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React, { useState } from "react";
 import Home from "./pages/home";
 import History from "./pages/history";
 // import Footer from "./components/footer";
-// import Header from "./components/header";
+import Header from "./components/header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import API from "./utils/API";
+
 import "./App.css";
 
 function App() {
   const [searchResult, setSearchResult] = useState([]);
   const [history, setHistory] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearchClick(event) {
+    event.preventDefault();
+
+    setHistory(history.concat(searchText));
+
+    if (searchText !== "") {
+      API.search(searchText)
+        .then(res => {
+          setSearchResult(res.data.hits);
+        })
+        .catch(err => console.log("err", err));
+    }
+  }
 
   return (
     <div>
       <Router>
-        {/* <Header /> */}
+        <Header />
         <Switch>
           <Route exact path={["/", "/home"]}>
             <Home
+              API={API}
+              handleSearchClick={handleSearchClick}
+              setSearchText={setSearchText}
               searchResult={searchResult}
               setSearchResult={setSearchResult}
-              history={history}
-              setHistory={setHistory}
             />
           </Route>
           <Route exact path={["/history"]}>
