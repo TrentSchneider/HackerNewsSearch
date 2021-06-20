de7f9a5683eaac2d4cd1734ba5f99b19a74be03a
TrentSchneider <trentonschneider@gmail.com>
2021-06-20 17:14:43 -0400

    Added sort button component

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchHistory from "../../components/searchHistory";
-// import Moment from "moment";
+// import HistorySort from "../../components/histSort";
 
 function History(props) {
   return (
     <div className="mx-1">
       <h2>History</h2>
+      <div>
+      <h3>Sort:</h3>
+      <button className="btn btn-secondary mx-1" onClick={props.sortAsc}>
+        Ascending
+      </button>
+      <button className="btn btn-secondary mx-1" onClick={props.sortDes}>
+        Descending
+      </button>
+    </div>
       <div className="card p-1 mt-4">
         <h2>Results:</h2>
         <SearchHistory history={props.history} />
