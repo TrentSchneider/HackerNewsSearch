790705b705112dda5d689d4deaaf66ad66db2dad
TrentSchneider <trentonschneider@gmail.com>
2021-06-20 17:15:25 -0400

    Created functions to sord history array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./pages/home";
 import History from "./pages/history";
 // import Footer from "./components/footer";
@@ -12,10 +12,15 @@ function App() {
   const [history, setHistory] = useState([]);
   const [searchText, setSearchText] = useState("");
 
+  useEffect(() => {
+    console.log("history updated");
+    console.log(history);
+  }, [history]);
+
   function handleSearchClick(event) {
     event.preventDefault();
 
-    setHistory(history.concat({ "search": searchText, "timestamp": new Date() }));
+    setHistory(history.concat({ search: searchText, timestamp: new Date() }));
 
     if (searchText !== "") {
       API.search(searchText)
@@ -24,6 +29,19 @@ function App() {
         })
         .catch(err => console.log("err", err));
     }
+  }
+
+  function sortDes() {
+    console.log("before d", history);
+    const desc = history.sort((a, b) => a.timestamp - b.timestamp);
+    console.log("after d", desc);
+    setHistory(desc);
+  }
+  function sortAsc() {
+    console.log("before a", history);
+    const asc = history.sort((a, b) => b.timestamp - a.timestamp);
+    console.log("after a", asc);
+    setHistory(asc);
   }
 
   return (
@@ -41,7 +59,12 @@ function App() {
             />
           </Route>
           <Route exact path={["/history"]}>
-            <History history={history} />
+            <History
+              history={history}
+              setHistory={setHistory}
+              sortDes={sortDes}
+              sortAsc={sortAsc}
+            />
           </Route>
         </Switch>
         {/* <Footer /> */}
