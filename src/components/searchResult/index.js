6e695dc861a5fb1aa6b61c142db5e70521ebfe0d
TrentSchneider <trentonschneider@gmail.com>
2021-06-22 12:03:04 -0400

    Updated search results to not create empty links when no URL is provided for search result

--- src/components/searchResult/index.js
+++ src/components/searchResult/index.js
@@ -8,17 +8,27 @@ function SearchResult(props) {
       </div>
     );
   } else {
+    console.log("results", props.searchResult);
     return (
       <div>
         {props.searchResult.map((data, i) => {
-          return (
-            <div key={i} className="card my-1 p-2">
-              <a href={data.url} target="_blank" rel="noreferrer">
+          if (data.url === null) {
+            return (
+              <div key={i} className="card my-1 p-2">
                 <h4 className="card-title">{data.title}</h4>
-              </a>
-              <p className="card-subtitle">Author: {data.author}</p>
-            </div>
-          );
+                <p className="card-subtitle">Author: {data.author}</p>
+              </div>
+            );
+          } else {
+            return (
+              <div key={i} className="card my-1 p-2">
+                <a href={data.url} target="_blank" rel="noreferrer">
+                  <h4 className="card-title">{data.title}</h4>
+                </a>
+                <p className="card-subtitle">Author: {data.author}</p>
+              </div>
+            );
+          }
         })}
       </div>
     );
