2fa58e78ccddfcf8bd8b6915b2833a2f1d7cb61f
TrentSchneider <trentonschneider@gmail.com>
2021-06-22 13:04:39 -0400

    Updated button styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,10 @@ import "./style.css";
 function Header() {
   return (
     <div className="headerBackground mb-2">
-      <Link to="/" className="btn m-1">
+      <Link to="/" className="btn btn-success m-1">
         Home
       </Link>
-      <Link to="/history" className="btn m-1">
+      <Link to="/history" className="btn btn-success m-1">
         History
       </Link>
     </div>
