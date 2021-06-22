06b3ae004c5cb423b9272bda30f2fe9054108df5
TrentSchneider <trentonschneider@gmail.com>
2021-06-22 13:17:39 -0400

    Added page title to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,13 +3,16 @@ import "./style.css";
 
 function Header() {
   return (
-    <div className="headerBackground mb-2">
-      <Link to="/" className="btn btn-success m-1">
-        Home
-      </Link>
-      <Link to="/history" className="btn btn-success m-1">
-        History
-      </Link>
+    <div className="headerBackground mb-2 row">
+      <div className="col-4">
+        <Link to="/" className="btn btn-success m-1 my-2">
+          Home
+        </Link>
+        <Link to="/history" className="btn btn-success m-1 my-2">
+          History
+        </Link>
+      </div>
+      <h1 className="col-4 text-center">Hacker News</h1>
     </div>
   );
 }
