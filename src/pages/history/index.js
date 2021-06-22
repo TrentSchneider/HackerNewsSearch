cadaaa9b8a9e25692b739640b2410b4954ee468c
TrentSchneider <trentonschneider@gmail.com>
2021-06-22 15:40:23 -0400

    Adjusted onClick events for sorting to now just update sort state

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,41 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import SearchHistory from "../../components/searchHistory";
 // import HistorySort from "../../components/histSort";
 
 function History(props) {
-  console.log("history", props.history);
+  const [sort, setSort] = useState("ascending");
+
   return (
     <div className="mx-1">
       <h1>History</h1>
       <h2>Sort:</h2>
       <button
         className="btn btn-secondary mx-1"
-        onClick={() => {
-          props.setHistory(props.sortAsc);
-        }}
+        onClick={() => setSort("ascending")}
       >
         Ascending
       </button>
       <button
         className="btn btn-secondary mx-1"
-        onClick={() => {
-          props.setHistory(props.sortDes);
-        }}
+        onClick={() => setSort("descending")}
       >
         Descending
       </button>
 
-      <button
-        className="btn btn-secondary mx-1"
-        onClick={props.clearStorage}
-      >
+      <button className="btn btn-secondary mx-1" onClick={props.clearStorage}>
         Clear History
       </button>
 
       <div className="card p-1 mt-4">
         <h2>Results:</h2>
-
-        <SearchHistory history={props.history} />
+        <SearchHistory history={props.history} sort={sort} />
       </div>
     </div>
   );
